c27c8d48f61f9a2443e6a98f8407f589204ad5f7
Mohamed elsayed <mohamedelsayed@Mohameds-MacBook-Pro.local>
2024-06-09 13:16:39 +0200

    -Adding the omniauth initzalizer -changing the name of the column

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_08_232130) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_09_001553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "uuid"
+    t.string "uid"
     t.string "provider"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
